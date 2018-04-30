3ae051343227cbfd13b5aef304a013e87ef8f8d8
Anya <hannakilpatrick@gmail.com>
2018-04-30 13:26:33 -0400

    determining user's best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app){
     //displays home page
     app.get("/api/friends", function(req, res){
         var users = require(path.join(__dirname, "../data/friends.js"));
-        console.log(users);
+        // console.log(users);
         res.json(users);
     });
     app.post("/api/friends", function(req, res){
@@ -14,6 +14,35 @@ module.exports = function(app){
         // This works because of our body-parser middleware
         var users = require(path.join(__dirname, "../data/friends.js"));
         var newUser = req.body;
+        
+        //checking compatibility
+        var differences = []; //array of totalDifferences
+        var totalDifference=0;
+        var score=0;
+        for (var m=0;m<users.length;m++){//looping through users array
+            for (var i = 0; i<newUser.scores.length; i++){//looping through each user's scores array 
+                var difference = Math.abs(users[m].scores[i] - newUser.scores[i]);
+                totalDifference += difference; //adding results after each match
+                score++;
+                if(score === 10){ //when score=10, it means that we looped through all scores of one user, and next 10 numbers will be from another user
+                    var match = {
+                        userIndex: m, //userIndex proroperty hold the user index, so after sorting numbers we will know which user this object relates to
+                        scoreDifference: totalDifference
+                    }
+                    differences.push(match);//pushing sum to array
+                    totalDifference=0;//setting back to 0
+                    score=0;
+                }
+            }
+        }
+        console.log("Array: "+ JSON.stringify(differences));
+        differences.sort(function(a, b) { //sorting objects in an array in ascending order of scoreDifference
+            return parseFloat(a.scoreDifference) - parseFloat(b.scoreDifference);
+        });
+        console.log("Best match "+ JSON.stringify(differences[0]));//first object is the best match as it has the lowest totalDifference
+        var bestMatchUserId = differences[0].userIndex;
+        // module.exports = users[bestMatchUserId];
+        //we are pushing newUser to array of users after we checked compatibility
         users.push(newUser);
         res.json(users);
     })
