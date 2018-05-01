587177b5db848b3884dc012a7eb22f23f0b249ea
Anya <hannakilpatrick@gmail.com>
2018-05-01 13:24:42 -0400

    changes the order of the routes requiring

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(bodyParser.json());
 
 var PORT = process.env.PORT||8080;
 //requiring module
-require(path.join(__dirname, "/app/routing/htmlRoutes.js"))(app);
-require(path.join(__dirname, "/app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, "/app/routing/apiRoutes.js"))(app); //requiring apiRoutes first, because htmlRoutes have a default route to home page
+require(path.join(__dirname, "/app/routing/htmlRoutes.js"))(app); 
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
